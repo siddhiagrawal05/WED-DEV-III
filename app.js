a5adcb8485f6e990082e2e359c054515e5e51af2
Siddhi Agrawal <siddhiagr05a@gmail.com>
2026-08-15 19:53:06 +0530

    6 aug backend work

---
+++ app.js
@@ -0,0 +1,29 @@
+//const cities = require('./data/data.js');
+//console.log(cities);
+//const fs = require('fs');
+//fs.writeFileSync("text.txt", "This is a text file created using Node.js");//to read ,create , update and delete files importing fs module
+        //const path = require('path');
+       // const filename = path.join(__dirname, 'text.txt');
+  //console.log(filename);
+// const os = require('os');
+// const userinfo = os.userInfo();
+// const freememmory = os.freemem();
+// const totalmemmory = os.totalmem();
+// const uptime = os.uptime();
+// console.log(userinfo);
+// console.log(`Free Memory: ${freememmory}`);
+// console.log(`Total Memory: ${totalmemmory}`);        
+// console.log("Uptime:", uptime);
+//-----------------------------------------------------------------------------------------
+//chalk k bare m install kiya h chalk
+// import chalk from 'chalk';
+// console.log(chalk.blue('Hello world!'));
+// console.log(chalk.red('This is an error message.'));
+// console.log(chalk.green('Success!'));
+// console.log(chalk.yellow('Warning!'));// common js m chalk working nhi h y sirf v 6 modules m chalengi
+// common js m chalk working nhi h y sirf vS6 modules m chalengi 
+
+// day 2 
+// crypto module is uswd for security 
+//crypto for password 
+//dns k barre m it converts domain names into ip adress 
---
+++ data/data.js
@@ -0,0 +1,13 @@
+const cities = [
+  {
+    name: "New York",
+    country: "USA",
+    population: 8419600,
+  },
+  {
+    name: "London",
+    country: "UK",
+    population: 9426000,
+  },
+];  
+module.exports = cities;
